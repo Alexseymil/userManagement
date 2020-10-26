b95ff58d879254ba806361e29d71c2439d92aecd
alexseymil <alexseymil@gmail.com>
2020-10-26 11:38:11 +0200

    'e'

--- UserManagement/src/main/java/com/management/model/UserAccount.java
+++ UserManagement/src/main/java/com/management/model/UserAccount.java
@@ -9,6 +9,7 @@ import java.util.Date;
 public class UserAccount {
 //New comment
     //new modification
+    //new modification2
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
 //@JsonIgnore()
