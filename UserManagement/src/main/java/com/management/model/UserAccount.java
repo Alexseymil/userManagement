f16c7b3eb547bbb72abc9c76017b9ed82951ddd0
Alexseymil <Alexseymil@gmail.com>
2020-10-26 11:39:06 +0200

    Update UserAccount.java
    x

--- UserManagement/src/main/java/com/management/model/UserAccount.java
+++ UserManagement/src/main/java/com/management/model/UserAccount.java
@@ -9,6 +9,7 @@ import java.util.Date;
 public class UserAccount {
 //New comment
     //new modification
+    //new difrent modification 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
 //@JsonIgnore()
